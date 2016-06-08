a66f3b142fcff9de0c1caa85d9d5711feda400a9
mperdikeas <mperdikeas@gmail.com>
2016-06-08 10:18:20 +0300

    first release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # faker-datastore
 thin wrapper of faker module to simulate a datastore with consistent results
+
+To use it:
+
+```javascript
+const FakeDataStore = require('faker-datastore');
+const ds = new FakeDataStore(100);
+ds.getSize(); // 100
+ds.get(-1); // null
+ds.get(100); // null
+ds.get(13); // consistently fetches the same piece of data simulating a datastore
+```
+
+const 
+
+
+## Tests
+
+    // not yet implemented
+
+## Contributing
+
+
+
+## Release History
+
+* 0.1.0 &nbsp;&nbsp;&nbsp; Initial release
---
+++ src/index.js
@@ -0,0 +1,59 @@
+const faker = require('Faker');
+
+
+class Address {
+    constructor(usState, city, streetName, streetAddress) {
+        this.usState       = usState;
+        this.city          = city;
+        this.streetName    = streetName;
+        this.streetAddress = streetAddress;
+    }
+}
+
+
+class Person {
+    constructor(fname, lname, email, dateOfBirth, address) {
+        this.fname       = fname;
+        this.lname       = lname;
+        this.email       = email;
+        this.dateOfBirth = dateOfBirth;
+        this.address     = address;
+    }
+}
+
+class FakeDataStore {
+    
+    constructor(n) {
+        this.n = n;
+        this.data  = [];
+    }
+
+    getSize() {
+        return this.n;
+    }
+
+    get(idx) {
+        if ((idx < 0) || (idx >= this.n))
+            return null;
+        if (!this.data[idx]) {
+            const address = new Address(faker.Address.usState()
+                                        , faker.Address.city()
+                                        , faker.Address.streetName()
+                                        , faker.Address.streetAddress());
+            const person = new Person(faker.Name.firstName()
+                                      , faker.Name.lastName()
+                                      , faker.Internet.email()
+                                      , faker.Date.recent(365*100)
+                                      , address);
+            this.data[idx] = person;
+        }
+        if (!this.data[idx])
+            throw new Error();
+        return this.data[idx];
+    }
+
+
+}
+
+
+module.exports = FakeDataStore;
